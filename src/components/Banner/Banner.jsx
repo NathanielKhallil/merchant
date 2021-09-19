c800405d2df7dbe54971dd6e230f0d6cd7950bec
NathanielKhallil <nathanielkhallil@gmail.com>
2021-09-18 21:16:50 -0600

    Style changes

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import bannerImage from "../../assets/Ghostpine_Banner.jpg";
+
+function Banner() {
+  return (
+    <div>
+      <img src={bannerImage} alt="commerce.js" width="100%" />
+    </div>
+  );
+}
+
+export default Banner;
