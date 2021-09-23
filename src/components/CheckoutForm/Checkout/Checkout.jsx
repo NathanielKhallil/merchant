9178c15e6bbe1563de35c2b26f26e57b8e6b6ce3
NathanielKhallil <78049880+NathanielKhallil@users.noreply.github.com>
2021-09-23 16:57:51 -0600

    WIP - Checkout, shipping

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,19 +1,46 @@
-import React, { useState } from 'react'
-import { paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button, Paper} from '@material-ui/core'
+import React, { useState, useEffect } from 'react'
+import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button,} from '@material-ui/core'
 
+
+import { commerce } from '../../../lib/commerce'
 import useStyles from "./styles";
+import AddressForm from '../AddressForm';
+import PaymentForm from '../PaymentForm';
 
 const steps = ['Shipping address', 'Payment details']
-function Checkout() {
+function Checkout({ cart }) {
     const classes = useStyles();
     const [activeStep, setActiveStep] = useState(0);
+
+    useEffect(() => {
+       const generateToken = async () => {
+        try {
+            const token = await commerce.checkout.generateToken(cart.id, {type: 'cart'})
+
+            console.log(token);
+        }catch(error) {
+
+        }
+    }  
+    }, []);
+
+    const Confirmation = () => (
+        <div>
+            Confirmation
+        </div>
+    );
+
+    const Form = () => activeStep === 0 
+    ? <AddressForm />
+    : <PaymentForm />
+
     return (
         <>
         <div className={classes.toolbar} />
         <main className={classes.layout}>
         <Paper className={classes.paper}>
             <Typography variant='h4' align="center"> Checkout</Typography>
-                <Stepper activeStep={0} className={classes.stepper}>
+                <Stepper activeStep={activeStep} className={classes.stepper}>
                 {steps.map((step) => (
                     <Step key={step}>
                         <StepLabel>{step}</StepLabel>
@@ -21,6 +48,7 @@ function Checkout() {
                 ))}
 
                 </Stepper>
+                {activeStep === steps.length ? <Confirmation /> : <Form />}
         </Paper>
         </main>
         </>
