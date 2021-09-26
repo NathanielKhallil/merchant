b7b9cdd9514aa3048482b8e0f13e545c3e3ba582
NathanielKhallil <nathanielkhallil@gmail.com>
2021-09-26 14:55:13 -0600

    variants added

--- src/components/Products/Product/MerchItem.jsx
+++ src/components/Products/Product/MerchItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Grid, Button, Typography, Container } from "@material-ui/core";
 import { ShoppingCart } from "@material-ui/icons";
 import { commerce } from "../../../lib/commerce";
-import Dropdown from "./Dropdown";
+import { Select, MenuItem } from "@material-ui/core";
 
 import { useState, useEffect } from "react";
 
@@ -12,8 +12,10 @@ const createMarkup = (text) => {
 
 const MerchItem = ({ handleAddToCart }) => {
   const [product, setProduct] = useState({});
+  const [variant, setVariant] = useState([]);
   const [quantity, setQuantity] = useState(1);
-  
+  const [sizeOption, setSizeOption] = useState([]);
+  const [variantGroup, setVariantGroup] = useState();
 
   const fetchProduct = async (id) => {
     const response = await commerce.products.retrieve(id);
@@ -27,17 +29,39 @@ const MerchItem = ({ handleAddToCart }) => {
       description,
       src: media.source,
       price: price.formatted_with_symbol,
-      variant_groups,
+      variant_groups: variant_groups,
     });
-    
+  };
+
+  const getVariants = async (id) => {
+    const data = await commerce.products.retrieve(id);
+
+    let option = data.variant_groups[0].options.map((element) => ({
+      id: element.id,
+      name: element.name,
+    }));
+    setVariant(option);
+  };
+
+  const completeVariant = async (id, size) => {
+    let variantGroup = {};
+    const data = await commerce.products.retrieve(id);
+    variantGroup[data.variant_groups[0].id] = size;
+
+    setVariantGroup(variantGroup);
   };
 
   useEffect(() => {
     const id = window.location.pathname.split("/");
     fetchProduct(id[2]);
+    getVariants(id[2]);
   }, []);
 
-   
+  useEffect(() => {
+    const id = window.location.pathname.split("/");
+    if (sizeOption) completeVariant(id[2], sizeOption);
+  }, [sizeOption]);
+
   const handleQuantity = (param) => {
     if (param === "decrease" && quantity > 1) {
       setQuantity(quantity - 1);
@@ -67,37 +91,70 @@ const MerchItem = ({ handleAddToCart }) => {
             dangerouslySetInnerHTML={createMarkup(product.description)}
           />
           <Typography variant="h6">
-            <table variant='h6' style={{textAlign: "justify"}}>
-            <caption colSpan="5" style={{textAlign: "left"}}>Select your size at checkout!</caption>
-                <thead variant="caption">
-                    <tr style={{textAlign: "center"}}>
-                    <th style={{textAlign: "left", width:'4rem'}}> Size(In):</th>
-                    <th variant='h6' style={{width: "4rem"}}>S</th> 
-                    <th variant='h6' >M</th>
-                    <th variant='h6' >L</th>
-                    <th variant='h6' >XL</th>
-                    </tr>
-                </thead>
-                   
-                <tbody>
-                    <tr style={{textAlign: "center", fontSize:'1rem'}}>
-                        <td style={{textAlign: "left"}}><strong>width:</strong></td>
-                        <td>18</td> 
-                        <td>20</td>
-                        <td>22</td>
-                        <td>24</td>
-                    </tr> 
-                    
-                    <tr style={{textAlign: "center", fontSize:'1rem'}}>
-                        <td style={{textAlign: "left"}}> <strong>height:</strong> </td>
-                        <td> 28 </td>
-                        <td> 29 <sup>1</sup>&frasl;<sub>4</sub> </td>
-                        <td> 30 <sup>1</sup>&frasl;<sub>4</sub> </td>
-                        <td> 31 <sup>1</sup>&frasl;<sub>4</sub> </td>
-                    </tr>
-                  </tbody>                     
-                </table>
-                <Dropdown />
+            <table variant="h6" style={{ textAlign: "justify" }}>
+              <caption colSpan="5" style={{ textAlign: "left" }}>
+                Select your size at checkout!
+              </caption>
+              <thead variant="caption">
+                <tr style={{ textAlign: "center" }}>
+                  <th style={{ textAlign: "left", width: "4rem" }}>
+                    {" "}
+                    Size(In):
+                  </th>
+                  <th variant="h6" style={{ width: "4rem" }}>
+                    S
+                  </th>
+                  <th variant="h6">M</th>
+                  <th variant="h6">L</th>
+                  <th variant="h6">XL</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr style={{ textAlign: "center", fontSize: "1rem" }}>
+                  <td style={{ textAlign: "left" }}>
+                    <strong>width:</strong>
+                  </td>
+                  <td>18</td>
+                  <td>20</td>
+                  <td>22</td>
+                  <td>24</td>
+                </tr>
+
+                <tr style={{ textAlign: "center", fontSize: "1rem" }}>
+                  <td style={{ textAlign: "left" }}>
+                    {" "}
+                    <strong>height:</strong>{" "}
+                  </td>
+                  <td> 28 </td>
+                  <td>
+                    {" "}
+                    29 <sup>1</sup>&frasl;<sub>4</sub>{" "}
+                  </td>
+                  <td>
+                    {" "}
+                    30 <sup>1</sup>&frasl;<sub>4</sub>{" "}
+                  </td>
+                  <td>
+                    {" "}
+                    31 <sup>1</sup>&frasl;<sub>4</sub>{" "}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+
+            <Select
+              value={sizeOption}
+              fullWidth
+              onChange={(e) => setSizeOption(e.target.value)}
+            >
+              {variant.map((element) => (
+                <MenuItem key={element.name} value={element.id}>
+                  {element.name}
+                </MenuItem>
+              ))}
+            </Select>
+            <p></p>
           </Typography>
 
           <Typography variant="h6"> Price: {product.price}</Typography>
@@ -131,7 +188,7 @@ const MerchItem = ({ handleAddToCart }) => {
             <Button
               size="large"
               onClick={() => {
-                handleAddToCart(product.id, quantity, product.variant_groups);
+                handleAddToCart(product.id, quantity, variantGroup);
               }}
             >
               <ShoppingCart /> Add to Cart
@@ -144,4 +201,3 @@ const MerchItem = ({ handleAddToCart }) => {
 };
 
 export default MerchItem;
-
