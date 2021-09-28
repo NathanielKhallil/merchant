263a7404120da3fb6c581371973ae1815aa996a8
NathanielKhallil <78049880+NathanielKhallil@users.noreply.github.com>
2021-09-28 16:59:44 -0600

    Added Stripe, near completion

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link, useHistory } from 'react-router-dom';
 import {
   Paper,
   Stepper,
@@ -16,11 +17,12 @@ import AddressForm from "../AddressForm";
 import PaymentForm from "../PaymentForm";
 
 const steps = ["Shipping address", "Payment details"];
-function Checkout({ cart }) {
+function Checkout({ cart, order, onCaptureCheckout, error }) {
   const classes = useStyles();
   const [activeStep, setActiveStep] = useState(0);
   const [checkoutToken, setCheckoutToken] = useState(null);
   const [shippingData, setShippingData] = useState({});
+  const history = useHistory();
 
   useEffect(() => {
     const generateToken = async () => {
@@ -30,11 +32,13 @@ function Checkout({ cart }) {
         });
 
         setCheckoutToken(token);
-      } catch (error) {}
+      } catch (error) {
+          history.pushState('/');
+      }
     };
 
     generateToken();
-  }, [cart]);
+  }, [cart, history]);
 
   const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1);
   const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1);
@@ -44,16 +48,39 @@ function Checkout({ cart }) {
     nextStep();
   };
 
-  const Confirmation = () => <div>Confirmation</div>;
+  const Confirmation = () => order.customer ? (
+  <>
+    <div>
+      <Typography variant='h5'>Thank you for your purchase, {order.customer.firstname} {order.customer.lastname}!</Typography>
+      <Divider className={classes.divder}/>
+      <Typography variant='subtitle2'>Order ref: {order.customer_reference} ref</Typography>
+      <br />
+      <Button variant='outline' type='button' component={Link} to='/'>Back to Home</Button>
+    </div>
+    </>
+  ) : (
+    <div className={classes.spinner}>
+      <CircularProgress />
+    </div>
+  );
+
+  if(error) {
+    <>
+  <Typography variant='h5'>Error: {error}</Typography> 
+  <br />
+  <Button variant='outline' type='button' component={Link} to='/'>Back to Home</Button>
+    </>
+  }
 
   const Form = () =>
-    activeStep === 0 ? (
-      <AddressForm checkoutToken={checkoutToken} next={next} />
-    ) : (
+    activeStep === 0 ? (<AddressForm checkoutToken={checkoutToken} next={next} />) : 
+    (
       <PaymentForm
         shippingData={shippingData}
         checkoutToken={checkoutToken}
+        nextStep={nextStep}
         backStep={backStep}
+        onCaptureCheckout={onCaptureCheckout}
       />
     );
 
