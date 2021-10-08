d6d76510d6a2a1517fea57e26c6585edc49623ee
NathanielKhallil <nathanielkhallil@gmail.com>
2021-10-07 20:07:59 -0600

    Tax functional!

--- src/components/CheckoutForm/Review.jsx
+++ src/components/CheckoutForm/Review.jsx
@@ -1,10 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Typography, List, ListItem, ListItemText } from "@material-ui/core";
-import {commerce} from "../../lib/commerce";
 
-
-
-const Review = ({ checkoutToken }, {tokenUpdate}) => {
+const Review = ({ checkoutToken, tokenUpdate }) => {
   console.log(tokenUpdate);
   return (
     <div>
@@ -12,10 +9,6 @@ const Review = ({ checkoutToken }, {tokenUpdate}) => {
         Order Summary
       </Typography>
       <List disablePadding>
-      {!tokenUpdate &&
-        <Typography variant="h6" gutterBottom>
-          Loading....
-          </Typography>}
         {checkoutToken.live.line_items.map((product) => (
           <ListItem style={{ padding: "10px 0" }} key={product.name}>
             <ListItemText
@@ -25,31 +18,25 @@ const Review = ({ checkoutToken }, {tokenUpdate}) => {
             <Typography variant="body2">
               {product.line_total.formatted_with_symbol}
             </Typography>
-
-
           </ListItem>
         ))}
-      
+
         <ListItem style={{ padding: "10px 0" }}>
-        <ListItemText primary="Tax" />
-        <Typography variant="body2">
-        {checkoutToken.live.tax.amount.formatted_with_symbol}
-      </Typography>
-      </ListItem>
-        
-        
+          <ListItemText primary="Tax" />
+          <Typography variant="body2">
+            {tokenUpdate.tax.amount.formatted_with_symbol}
+          </Typography>
+        </ListItem>
 
         <ListItem style={{ padding: "10px 0" }}>
           <ListItemText primary="Total" />
           <Typography variant="subtitle1" style={{ fontWeight: 700 }}>
-            {checkoutToken.live.total_with_tax.formatted_with_symbol}
+            {tokenUpdate.total_with_tax.formatted_with_symbol}
           </Typography>
         </ListItem>
       </List>
     </div>
-  )
-  
-
+  );
 };
 
 export default Review;
